6ce9ab784e1e8c9f0ed131e046b8355116a3046c
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-06-23 09:53:21 -0300

    fix: adds first letter with uppercase

--- stock-system/internal/market/entity/investor.go
+++ stock-system/internal/market/entity/investor.go
@@ -6,7 +6,7 @@ type Investor struct {
 	AssetPosition []*InvestorAssetPosition
 }
 
-func newInvestor(id string) *Investor {
+func NewInvestor(id string) *Investor {
 	return &Investor{
 		ID:            id,
 		AssetPosition: []*InvestorAssetPosition{},
