a95cc88e80414db117d73d940e8c73bd929e59c6
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-06-24 17:54:18 -0300

    feat: create order

--- back-end/src/orders/orders.controller.ts
+++ back-end/src/orders/orders.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { InitTransactionDto } from './order.dto';
 import { OrdersService } from './orders.service';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
+
+  @Post()
+  initTransactionDto(@Body() body: InitTransactionDto) {
+    return this.ordersService.initTransaction(body);
+  }
 }
--- back-end/src/orders/orders.service.ts
+++ back-end/src/orders/orders.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { OrderStatus } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
+import { InitTransactionDto } from './order.dto';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+  constructor(private prismaService: PrismaService) {}
+
+  initTransaction(input: InitTransactionDto) {
+    return this.prismaService.order.create({
+      data: {
+        asset_id: input.asset_id,
+        wallet_id: input.wallet_id,
+        shares: input.shares,
+        partial: input.shares,
+        price: input.price,
+        type: input.type,
+        status: OrderStatus.PENDING,
+      },
+    });
+  }
+}
