183df4f06037b3df2a756112551da440cc751970
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-06-24 16:09:40 -0300

    feat: create wallet

--- back-end/src/wallets/wallets.controller.ts
+++ back-end/src/wallets/wallets.controller.ts
@@ -4,4 +4,11 @@ import { WalletsService } from './wallets.service';
 @Controller('wallets')
 export class WalletsController {
   constructor(private readonly walletsService: WalletsService) {}
+
+  @Post()
+  create(@Body() body: { id: string }) {
+    return this.walletsService.create({
+      id: body.id,
+    });
+  }
 }
