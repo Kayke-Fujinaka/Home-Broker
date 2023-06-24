7cacfcadf7e3f87e6e9e20f87db2954a963cf98b
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-06-24 16:43:25 -0300

    feat: generate wallet assets

---
+++ back-end/src/wallets/wallet-assets/wallet-assets.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('wallet-assets')
+export class WalletAssetsController {}
--- back-end/src/wallets/wallets.module.ts
+++ back-end/src/wallets/wallets.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { WalletAssetsController } from './wallet-assets/wallet-assets.controller';
+import { WalletAssetsService } from './wallet-assets/wallet-assets.service';
 import { WalletsController } from './wallets.controller';
 import { WalletsService } from './wallets.service';
 
 @Module({
-  controllers: [WalletsController],
-  providers: [WalletsService],
+  controllers: [WalletsController, WalletAssetsController],
+  providers: [WalletsService, WalletAssetsService],
 })
 export class WalletsModule {}
