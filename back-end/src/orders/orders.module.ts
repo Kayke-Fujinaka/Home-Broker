d60880442a5d5b476be3153387105b0ce4b910e5
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-06-26 12:19:49 -0300

    config: setup kafka

--- back-end/src/orders/orders.module.ts
+++ back-end/src/orders/orders.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'ORDERS_PUBLISHER',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'orders',
+            brokers: ['host.docker.internal:9094'],
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
