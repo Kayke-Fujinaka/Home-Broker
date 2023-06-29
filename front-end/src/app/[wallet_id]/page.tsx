2fca0e2374d0ff8c59c7e07fb5a3dbb816c7be04
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-06-29 10:28:40 -0300

    feat: list assets

--- front-end/src/app/[wallet_id]/page.tsx
+++ front-end/src/app/[wallet_id]/page.tsx
@@ -1,12 +1,29 @@
-export default function HomePage({
+import { WalletAsset } from "../models";
+
+async function getWalletAssets(wallet_id: string): Promise<WalletAsset[]> {
+  const response =  await fetch(`http://localhost:8000/wallets/${wallet_id}/assets`)
+  return response.json()
+}
+
+export default async function HomePage({
   params,
 }: {
   params: { wallet_id: string };
 }) {
+
+  const walletAssets = await getWalletAssets(params.wallet_id)
+
   return (
     <div>
       <h1>Meus investimentos</h1>
-      <p>{params.wallet_id}</p>
+      <ul>
+        {walletAssets.map((walletAsset) => (
+          <li key={walletAsset.id}>
+            {walletAsset.Asset.id} - {walletAsset.shares} - R${" "}
+            {walletAsset.Asset.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
